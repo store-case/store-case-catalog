eaea086cbd7e480b8cffedea0ae414d9360d03ae
honee8583 <ekgns0508@gmail.com>
2025-10-22 14:51:31 +0900

    chore: init project

---
+++ src/main/java/com/leedahun/storecasecatalog/StoreCaseCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.leedahun.storecasecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCaseCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreCaseCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leedahun/storecasecatalog/StoreCaseCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leedahun.storecasecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreCaseCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
